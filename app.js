36d7ee0eb90677239072362ab42749e80bcc0c4c
Philipp Hancke <fippo@goodadvice.pages.de>
2014-01-14 20:50:11 +0100

    bump strophe.jingle version and make logging webrtc-internals importer compatible (even though it does not exist yet)

--- app.js
+++ app.js
@@ -441,24 +441,29 @@ $(window).bind('beforeunload', function () {
 
 function dump(elem, filename){
     elem = elem.parentNode;
-    elem.download = filename || 'xmpplog.json';
+    elem.download = filename || 'meetlog.json';
     elem.href = 'data:application/json;charset=utf-8,\n';
     var data = {};
-    data.time = new Date();
-    data.url = window.location.href;
-    data.ua = navigator.userAgent;
-    if (connection.logger) {
-        data.xmpp = connection.logger.log;
-    }
     if (connection.jingle) {
         Object.keys(connection.jingle.sessions).forEach(function (sid) {
             var session = connection.jingle.sessions[sid];
             if (session.peerconnection && session.peerconnection.updateLog) {
                 // FIXME: should probably be a .dump call
-                data["jingle_" + session.sid] = session.peerconnection.updateLog;
+                data["jingle_" + session.sid] = {
+                    updateLog: session.peerconnection.updateLog,
+                    url: window.location.href}
+                ;
             }
         });
     }
+    metadata = {};
+    metadata.time = new Date();
+    metadata.url = window.location.href;
+    metadata.ua = navigator.userAgent;
+    if (connection.logger) {
+        metadata.xmpp = connection.logger.log;
+    }
+    data.metadata = metadata;
     elem.href += encodeURIComponent(JSON.stringify(data, null, '  '));
     return false;
 }
